7e181d73cd38ddd4bfbe8f5d2e5bc2914a5867bf
varunlad <varunlad5@gmail.com>
2024-05-16 18:02:26 +0530

    frontend challenge

--- src/Components/HomeCoading.js
+++ src/Components/HomeCoading.js
@@ -4,36 +4,76 @@ import "./codingStyle.scss";
 function HomeCoading() {
   const [repositories, setRepositories] = useState([]);
   const [loader, setLoader] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentDropdownIndex, setCurrentDropdownIndex] = useState("");
   useEffect(() => {
+    repoList(1);
+  }, []);
+  const repoList = (pgNo) => {
     fetch(
-      "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=2"
+      "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=" +
+        pgNo
     )
       .then((res) => {
         return res.json();
       })
       .then((data) => {
-        if (data.items.length > 0) {
-          console.log(data.items);
+        if (data?.items?.length > 0) {
           setLoader(false);
           setRepositories(data?.items);
         } else {
           setLoader(false);
           setRepositories([]);
+          alert(data.message + " Please Refresh again!!!");
         }
       });
-  }, []);
+  };
+  const handelPage = (num) => {
+    setCurrentPage(currentPage + num);
+    repoList(currentPage + num);
+    setRepositories([]);
+    setLoader(true);
+  };
+  const handelDropdownIndex = (id) => {
+    setCurrentDropdownIndex(id);
+  };
   return (
     <div className="home_code">
       <div className="container">
-        <h1 className="mt-5">Coding Challenge</h1>
-        <div className="row mt-5">
+        <div className=" d-flex justify-content-between align-items-center ">
+          <h1>Coding Challenge</h1>
+          {!loader && (
+            <div className="d-flex">
+              {currentPage === 1 ? null : (
+                <button
+                  onClick={() => {
+                    handelPage(-1);
+                  }}
+                >
+                  Previous
+                </button>
+              )}
+              {/* {currentPage === 2 ? null : ( */}
+              <button
+                onClick={() => {
+                  handelPage(1);
+                }}
+              >
+                Next
+              </button>
+              {/* )} */}
+            </div>
+          )}
+        </div>
+
+        <div className="row ">
           {loader ? (
             <div>Loading...</div>
           ) : (
             repositories?.map((ele, ind) => {
               return (
                 <div key={ind} className="col-md-12">
-                  <div className="current_repo">
+                  <div className="current_repo mt-2">
                     <div className="row">
                       <div className="col-md-2">
                         <div className="profile_pic">
@@ -59,11 +99,23 @@ function HomeCoading() {
                           </div>
                         </div>
                       </div>
-                      <div className="col-md-1">
-                        <div className="go_down">
-                          <i class="bi bi-chevron-down"></i>
+                      {/* <div className="col-md-1">
+                        <div
+                          className="go_down"
+                          onClick={() => {
+                            handelDropdownIndex(ele.id);
+                          }}
+                        >
+                          {ele.id !== currentDropdownIndex ?
+                          <i class="bi bi-chevron-down"></i> :
+                          <i class="bi bi-chevron-up"></i> }
                         </div>
                       </div>
+                      {ele.id === currentDropdownIndex && (
+                        <div className="col-md-12">
+                          <h1>Dropdown</h1>
+                        </div>
+                      )} */}
                     </div>
                   </div>
                 </div>
